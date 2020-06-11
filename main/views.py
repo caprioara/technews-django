21584fb0be54887a388cac79b16c7bf3b7cdf124
caprioara <dragos.neatax@gmail.com>
2020-06-11 15:39:24 +0300

    114 Add new page

--- main/views.py
+++ main/views.py
@@ -3,7 +3,11 @@ from .models import Main
 
 
 def home(request):
-	template_name = "home.html"
+	template_name = "front/home.html"
 	return render(request, template_name)
 
+
+def about(request):
+	template_name = "front/about.html"
+	return render(request, template_name)
 
