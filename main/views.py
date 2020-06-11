68765e64b3c1bba6d4eb51c47b9afc5b9b5b361a
caprioara <dragos.neatax@gmail.com>
2020-06-11 10:46:17 +0300

    105 Test App

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Main
 
-# Create your views here.
+
+def home(request):
+	template_name = "home.html"
+	return render(request, template_name)
+
+
