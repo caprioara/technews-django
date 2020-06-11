304a5f73ff0de0695f1027083b7275967322db19
caprioara <dragos.neatax@gmail.com>
2020-06-11 16:47:52 +0300

    119 Query

--- main/views.py
+++ main/views.py
@@ -3,11 +3,20 @@ from .models import Main
 
 
 def home(request):
+
 	template_name = "front/home.html"
-	return render(request, template_name)
+	sitename = "Django | Home"
+	context = {'sitename':sitename, 'site':site}
+
+	site = Main.object.filter(pk=1)
+
+	return render(request, template_name, context)
 
 
 def about(request):
+
 	template_name = "front/about.html"
-	return render(request, template_name)
+	sitename = "Django | About"
+	context = {'sitename':sitename}
+	return render(request, template_name, context)
 
