973cb6804382724018259f1c63cedf6ab3e117a3
caprioara <dragos.neatax@gmail.com>
2020-06-13 16:11:20 +0300

    128 Settings up news detail page

--- news/views.py
+++ news/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News
+from main.models import Main
 
 def news_detail(request, pk):
 
 	template_name = "front/news_detail.html"
 
 	news = News.objects.filter(pk=pk)
+	site = Main.objects.get(pk=pk)
 
-	context = {'news':news}
+	context = {'news':news, 'site':site}
 
 	return render(request, template_name, context)
