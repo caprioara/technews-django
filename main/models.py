28aca104d3e979b5187d7c3637ae5e06979b97d1
caprioara <dragos.neatax@gmail.com>
2020-06-13 11:21:24 +0300

    124 Configur Model

--- main/models.py
+++ main/models.py
@@ -2,13 +2,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Main(models.Model):
-	name = models.TextField()
+	name = models.CharField(max_length=50)
 	about = models.TextField()
-	fb = models.TextField(default=" ")
-	tw = models.TextField(default=" ")
-	yt = models.TextField(default=" ")
 
-	set_name = models.TextField(default=" ")
+	fb = models.CharField(max_length=50)
+	tw = models.CharField(max_length=50)
+	yt = models.CharField(max_length=50)
+
+	tell = models.CharField(max_length=30)
+	link = models.CharField(max_length=90)
+
+	set_name = models.CharField(max_length=50)
 
 	def __str__(self):
 		return self.set_name + " | " + str(self.pk)
