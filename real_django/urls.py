e70ab4628128430f5d1474093f063179b10d8f42
caprioara <dragos.neatax@gmail.com>
2020-06-15 17:34:23 +0300

    Settings up_2

--- real_django/urls.py
+++ real_django/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+
+#for photos
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('main.urls')),
     url(r'', include('news.urls')),
 ]
+
+if settings.DEBUG:
+
+	urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
